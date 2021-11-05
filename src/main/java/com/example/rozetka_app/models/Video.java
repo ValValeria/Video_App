320427f9dea91896297d5b63638b34f87faf0042
ValValeria <led2017l@gmail.com>
2021-11-05 19:31:20 +0300

    small fix

--- src/main/java/com/example/rozetka_app/models/Video.java
+++ src/main/java/com/example/rozetka_app/models/Video.java
@@ -9,11 +9,55 @@ public class Video {
     @Column(name = "id", nullable = false)
     private Integer id;
 
+    @Column
+    private String path;
+
+    @Column
+    private String title;
+
+    @Column(columnDefinition = "date")
+    private String date;
+
+    @Column
+    private String description;
+
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
